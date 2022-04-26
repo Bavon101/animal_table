6900603a64d38f78df573305f7759508369ea1ff
Bavon101 <bavonfate@gmail.com>
2022-04-26 21:05:18 +0300

    Add query and update statements

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES(
-	' Agumon',
+	'Agumon',
 	'Feb 3, 2020',
 	0,
 	'1',
@@ -22,4 +22,50 @@ INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALU
 	5,
 	'1',
 	11.0
+);
+
+--Insert new data
+
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES(
+	'Charmander',
+	'Feb 8, 2020',
+	0,
+	'0',
+	-11
+),(
+	'Plantmon',
+	'Nov 15, 2021',
+	2,
+	'1',
+	-5.7
+),(
+	'Squirtle',
+	'Apr 2, 1993',
+	3,
+	'0',
+	-12.13
+),(
+	'Angemon',
+	'Jun 12, 2005',
+	1,
+	'1',
+	-45
+),(
+	'Boarmon',
+	'Jun 7, 2005',
+	7,
+	'1',
+	20.4
+),(
+	'Blossom',
+	'Oct 13, 1998',
+	3,
+	'1',
+	17
+),(
+	'Ditto',
+	'May 14, 2022',
+	4,
+	'1',
+	22
 );
